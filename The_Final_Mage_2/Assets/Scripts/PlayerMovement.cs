10bc62beafccb559a7939b0bd64ae30d13db2168
Shane Roberts <srober81@uncc.edu>
2016-02-03 16:04:37 -0500

    Added incredibly basic movement script using the arrow keys and a player object.

---
+++ The_Final_Mage_2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    public float speed = 1f;
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));//Gets our X and Y axis from raw input
+
+        rbody.MovePosition(rbody.position + movement_vector * speed * Time.deltaTime);
+    }
+}
