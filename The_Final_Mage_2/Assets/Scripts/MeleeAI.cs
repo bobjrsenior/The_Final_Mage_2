f45b8d3905172e6857fadac4d07c94af8e0671ee
Shane Roberts <srober81@uncc.edu>
2016-02-10 14:11:16 -0500

    Added a basic and horrific AI for melee enemy types. Will need significant work and heavy modification to become desirable, and is more meant for testing purposes.

---
+++ The_Final_Mage_2/Assets/Scripts/MeleeAI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAI : MonoBehaviour {
+
+    /// <summary>
+    /// The move speed for the enemy.
+    /// </summary>
+    public float enemySpeed = 1f;
+
+    /// <summary>
+    /// The rigidBody2D object attached to the enemy.
+    /// </summary>
+    private Rigidbody2D self;
+
+    /// <summary>
+    /// The rigidBody2D object attached to the player.
+    /// </summary>
+    private Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+
+        //Sets the self rigidbody2D object to the one this script is attached to.
+        self = GetComponent<Rigidbody2D>();
+        
+        //Sets the player rigidbody2D object to the gameObject that the playerMovement script is attached to(AKA, the player)
+        player = GameObject.FindObjectOfType<PlayerMovement>().GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Moves the object this script is attached to toward the player. THIS WILL HAVE TO BE CHANGED OR MODIFIED HEAVILY TO WORK BETTER.
+        transform.position = Vector2.MoveTowards(self.position, player.position, enemySpeed * Time.fixedDeltaTime);
+
+
+	}
+}
