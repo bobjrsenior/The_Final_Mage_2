632ab25f00093fed4bb22bab2e7eaa1bf2bd34d4
Shane Roberts <srober81@uncc.edu>
2016-02-12 12:49:03 -0500

    Revert "Fixed a bug with the playerRadius script that prevented it from functioning after implementing the new animations. Still receiving a getComponent error in the console that I cannot solve."
    This reverts commit 63ff6536e10c3a704e6aa84555406c1c6d60253b.

--- The_Final_Mage_2/Assets/Scripts/PlayerRadius.cs
+++ The_Final_Mage_2/Assets/Scripts/PlayerRadius.cs
@@ -19,7 +19,7 @@ public class PlayerRadius : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //This moves the object attached to the PlayerRadius script to the same position as the player at every frame, ensuring the radius is always centered on the player.
-        transform.position = player.GetComponent<Rigidbody2D>().position;
+        GetComponent<Rigidbody2D>().MovePosition(player.GetComponent<Rigidbody2D>().position);
 
 	}
 
