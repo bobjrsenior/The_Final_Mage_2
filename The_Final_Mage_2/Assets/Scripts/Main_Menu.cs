ef39f1cdb1ff3743e7153b154bba76337efd6fa7
Mohamed Salad <msalad@uncc.edu>
2016-02-15 16:32:13 -0500

    Menu Scene added
    Added test menu at the moment still working on responding to onclick
    and changing scene will be added soon

---
+++ The_Final_Mage_2/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour {
+
+	public string startLevel;
+	public string levelSelect;
+
+	public void newGame ()
+	{
+		Application.LoadLevel (startLevel);
+	}
+
+	//public void levelSelect()
+//	{
+//
+//	}
+
+}
