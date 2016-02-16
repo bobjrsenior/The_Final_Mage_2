569291efa3b8b6647c95b3b8aa66ac6bfc636898
Shane Roberts <srober81@uncc.edu>
2016-02-15 21:21:08 -0500

    Overhauled the EnemyAI script to make ranged type enemy movement and melee type enemy movement more optimal. Ranged type enemies will no longer shake when they are at the threshold of acceptable distance from the player and melee enemies will now stop when they touch the player so they do not push them around any longer.

--- The_Final_Mage_2/Assets/Scripts/EnemyAI.cs
+++ The_Final_Mage_2/Assets/Scripts/EnemyAI.cs
@@ -23,6 +23,11 @@ public class EnemyAI : MonoBehaviour {
     /// </summary>
     private Rigidbody2D player;
 
+    /// <summary>
+    /// Tells the enemy whether or not it is allowed to move (for melee types to prevent pushing the player around)
+    /// </summary>
+    public bool noMove;
+
 	// Use this for initialization
 	void Start () {
 
@@ -44,13 +49,18 @@ public class EnemyAI : MonoBehaviour {
             //If we are in the players detection zone(and therefore, allowed to move)
             if (self.inRadius == true)
             {
-                //Get the direction to the player
-                Vector2 direction = player.transform.position - transform.position;
-                //Normalize it so that the magnitude is 1 (5 * direction gives a total value of 5 in that direction)
-                direction.Normalize();
+                //If the enemy is allowed to move...
+                if (noMove == false)
+                {
+                    Debug.Log(noMove);
+                    //Get the direction to the player
+                    Vector2 direction = player.transform.position - transform.position;
+                    //Normalize it so that the magnitude is 1 (5 * direction gives a total value of 5 in that direction)
+                    direction.Normalize();
 
-                //Move towards player at enemySpeed units/second
-                selfRigid.MovePosition(selfRigid.position + direction * enemySpeed * Time.fixedDeltaTime);
+                    //Move towards player at enemySpeed units/second
+                    selfRigid.MovePosition(selfRigid.position + direction * enemySpeed * Time.fixedDeltaTime);
+                }
             }
         }
         else if (self.rangedType == true)
@@ -62,13 +72,19 @@ public class EnemyAI : MonoBehaviour {
                 Vector2 direction = player.transform.position - transform.position;
 
                 //If the distance between our player and the enemy attached to this script is less than two...
-                if (Vector2.Distance(player.transform.position, selfRigid.position) > 2)
+                if (Vector2.Distance(player.transform.position, selfRigid.position) > 2.1)
                 {
                     //Normalize it so that the magnitude is 1 (5 * direction gives a total value of 5 in that direction)
                     direction.Normalize();
 
                     //Move towards player at enemySpeed units/second
                     selfRigid.MovePosition(selfRigid.position + direction * enemySpeed * Time.fixedDeltaTime);
+                }
+                //If the distance to the player is between 2.1 and 2
+                else if (Vector2.Distance(player.transform.position, selfRigid.position) <= 2.1 && Vector2.Distance(player.transform.position, selfRigid.position) >= 2)
+                {
+                    //Remain where we are.
+                    selfRigid.MovePosition(selfRigid.position);
                 }
                 else
                 {
@@ -78,8 +94,34 @@ public class EnemyAI : MonoBehaviour {
                     //Move away from the player at enemySpeed units/second
                     selfRigid.MovePosition(selfRigid.position - direction * enemySpeed * Time.fixedDeltaTime);
                 }
-
             }
         }
 	}
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        //This only applies to melee types, so if we are a ranged type, we will not worry about this.
+        if (self.rangedType == false)
+        {
+            //Debug.Log("Collision Detected with " + other.gameObject.name);
+            //If we have collided with the player
+            if (other.gameObject.tag == "Player")
+            {
+                //No more movement is allowed from the update function.
+                noMove = true;
+                //Ensures that the enemy will not move by moving their position to their current position.
+                selfRigid.MovePosition(selfRigid.position);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        //If we are no longer colliding with the player
+        if (other.gameObject.tag == "Player")
+        {
+            //Restore the update functions ability to move the enemy.
+            noMove = false;
+        }
+    }
 }
