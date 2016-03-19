0b7c0da3c2ee120e8834bf263b6b77c9a8e70aaa
Shane Roberts <srober81@uncc.edu>
2016-03-19 15:35:43 -0400

    Added in a more intense music that plays on floors 5 and 6, and modified the main camera sound source to handle BGM swapping.

--- The_Final_Mage_2/Assets/Scripts/Player/CameraScript.cs
+++ The_Final_Mage_2/Assets/Scripts/Player/CameraScript.cs
@@ -10,9 +10,20 @@ public class CameraScript : MonoBehaviour {
     /// </summary>
     public Transform initialPoint;
 
+    public SoundScript BGM;
+
 	// Use this for initialization
 	void Start () {
 
+        //Play the appropriate music for the last two floors.
+        if (DifficultyManager.dManager.floor >= 5)
+        {
+            BGM.PlaySound(1);
+        }
+        else
+        {
+            BGM.PlaySound(0);
+        }
         //Ensure that our camera warps to its initial position when it is first created.
         transform.position = new Vector3(3.0f, -3.0f, -10);
 
