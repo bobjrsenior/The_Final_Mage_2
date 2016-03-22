0e377adab10f7302af3fc31a473ba6ac9d309072
Shane Roberts <srober81@uncc.edu>
2016-03-22 18:08:59 -0400

    Added in a slower score degeneration skill for the time being. This is likely to be temporary and change later, but now brings us to 4 purchaseable skills in game.

--- The_Final_Mage_2/Assets/Scripts/Managerial/Scoring.cs
+++ The_Final_Mage_2/Assets/Scripts/Managerial/Scoring.cs
@@ -47,6 +47,8 @@ public class Scoring : MonoBehaviour {
 
     public static Scoring scoreKeeper;
 
+    private bool degenerationSkill;
+
     // Use this for initialization
     void Start () {
 
@@ -67,6 +69,13 @@ public class Scoring : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //Enact the degeneration skill if it is purchased.
+        if (Skills.pSkills.skill4 == true && degenerationSkill == false)
+        {
+            degenerationTimer.initialTime += .25f;
+            degenerationSkill = true;
+        }
 
         if (degenerationCooldown == false && score != 0)
         {
