efe0c0b7999136c6aa9b9550abf4a07200a185bf
Shane Roberts <srober81@uncc.edu>
2016-03-22 22:44:28 -0400

    Fireballs now have trailing material effects.

--- The_Final_Mage_2/Assets/Scripts/Player/Projectile.cs
+++ The_Final_Mage_2/Assets/Scripts/Player/Projectile.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+    public TrailRenderer trail;
+
     void Start()
     {
         //Shrinks sprite to the appropriate size.
         transform.localScale = new Vector3(.10f, .10f, transform.localScale.z);
+        trail.sortingOrder = 2;
     }
     void OnTriggerEnter2D(Collider2D other)
     {
