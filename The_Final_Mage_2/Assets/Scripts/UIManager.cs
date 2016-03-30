d77e23dafd9d54b873e666ca0481ee5417338b5e
Mohamed Salad <msalad@uncc.edu>
2016-03-30 16:56:33 -0400

    Updated Text UI to respond to user damage
    Instead of referencing the onGUI method, now we have a text loaded
    canvas for  responding to damage

---
+++ The_Final_Mage_2/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    Text txt;
+    public static UIManager uiManager;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        txt.text = "" + PlayerHealth.pHealth.health;
+	
+	}
+
+    
+}
