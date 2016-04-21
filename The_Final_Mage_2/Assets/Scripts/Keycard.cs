1ff196e4ff39265fed0342046e9dfb4524e50e11
Bobjrsenior <bobjrsenior@gmail.com>
2016-04-21 15:06:50 -0400

    Elevator now opens when you get the keycard

--- The_Final_Mage_2/Assets/Scripts/Keycard.cs
+++ The_Final_Mage_2/Assets/Scripts/Keycard.cs
@@ -3,11 +3,22 @@ using System.Collections;
 
 public class Keycard : MonoBehaviour {
 
+    /// <summary>
+    /// This floor's elevator
+    /// </summary>
+    private Elevator elevator;
+
+    void Awake()
+    {
+        elevator = GameObject.FindObjectOfType<Elevator>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             DifficultyManager.dManager.retrievedKeyCard();
+            elevator.open();
             Destroy(this.gameObject);
         }
     }
