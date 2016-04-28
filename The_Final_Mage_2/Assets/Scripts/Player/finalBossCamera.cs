184cb207d2260ed49cea45d6bc04d4db55792eac
Shane Roberts <srober81@uncc.edu>
2016-04-28 19:17:49 -0400

    final boss uses music

--- The_Final_Mage_2/Assets/Scripts/Player/finalBossCamera.cs
+++ The_Final_Mage_2/Assets/Scripts/Player/finalBossCamera.cs
@@ -8,6 +8,8 @@ public class finalBossCamera : MonoBehaviour {
     public void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        GetComponent<AudioSource>().clip = FindObjectOfType<SoundScript>().audioClip[4];
+        GetComponent<AudioSource>().Play();
     }
     public void Update()
     {
